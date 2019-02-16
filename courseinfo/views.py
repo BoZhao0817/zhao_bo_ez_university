a56d141c7de6a1c33dc78da3104c1fb24003be0e
Bo Zhao <boz5@illinois.edu>
2019-02-15 22:03:32 -0600

    finish template assignment

--- courseinfo/views.py
+++ courseinfo/views.py
@@ -5,6 +5,11 @@ from django.template import loader
 from .models import (
     Instructor,
     Section,
+    Course,
+    Semester,
+    Student,
+    Registration,
+
 )
 
 def instructor_list_view(request):
@@ -25,8 +30,40 @@ def instructor_list_view(request):
 
 def section_list_view(request):
     section_list = Section.objects.all()
+    #section_list = Section.objects.none()
     template = loader.get_template('courseinfo/section_list.html')
     context = {'section_list': section_list}
     output = template.render(context)
     return HttpResponse(output)
 
+def course_list_view(request):
+    course_list = Course.objects.all()
+    #course_list = Course.objects.none()
+    template = loader.get_template('courseinfo/course_list.html')
+    context = {'course_list': course_list}
+    output = template.render(context)
+    return HttpResponse(output)
+
+def semester_list_view(request):
+    semester_list = Semester.objects.all()
+    #semester_list = Semester.objects.none()
+    template = loader.get_template('courseinfo/semester_list.html')
+    context = {'semester_list': semester_list}
+    output = template.render(context)
+    return HttpResponse(output)
+
+def student_list_view(request):
+    student_list = Student.objects.all()
+    #student_list = Student.objects.none()
+    template = loader.get_template('courseinfo/student_list.html')
+    context = {'student_list': student_list}
+    output = template.render(context)
+    return HttpResponse(output)
+
+def registration_list_view(request):
+    registration_list = Registration.objects.all()
+    #registration_list = Registration.objects.none()
+    template = loader.get_template('courseinfo/registration_list.html')
+    context = {'registration_list': registration_list}
+    output = template.render(context)
+    return HttpResponse(output)
--- zhao_bo_ez_university/urls.py
+++ zhao_bo_ez_university/urls.py
@@ -3,14 +3,19 @@ from django.urls import path
 from courseinfo.views import (
     instructor_list_view,
     section_list_view,
+    course_list_view,
+    semester_list_view,
+    student_list_view,
+    registration_list_view,
+
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('instructor/', instructor_list_view),
     path('section/', section_list_view),
-    # course
-    # semester
-    # student
-    # registration
+    path('course/', course_list_view),
+    path('semester/', semester_list_view),
+    path('student/', student_list_view),
+    path('registration/', registration_list_view),
 ]
